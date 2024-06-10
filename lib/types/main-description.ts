471163d3bc372e25a599c82a95e125e4fbdbb1d2
Vi-lka <vitalya.permyakov155@gmail.com>
2024-06-10 16:00:09 +0700

    feat: main page query + fix lint

--- lib/types/main-description.ts
+++
@@ -1,8 +0,0 @@
-import { z } from "zod"
-import { ImagesArrayT } from "./main"
-
-export const MainDescriptionT = z.object({
-  description: z.string(),
-  icons: ImagesArrayT
-})
-export type MainDescriptionT = z.infer<typeof MainDescriptionT> 
