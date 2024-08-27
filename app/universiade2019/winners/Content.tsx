444c22c3cbf344c6c0f39348c2189a0ef67494dc
Vi-lka <vitalya.permyakov155@gmail.com>
2024-08-27 11:31:27 +0700

    fix: winners content

--- app/universiade2019/winners/Content.tsx
+++ app/universiade2019/winners/Content.tsx
@@ -1,8 +1,11 @@
 import ImageComponent from '@/components/ImageComponent'
+import PaginationControls from '@/components/PaginationControls'
 import ErrorHandler from '@/components/errors/ErrorHandler'
 import { getSportsmans } from '@/lib/queries/sportsman'
 import Link from 'next/link'
 import React from 'react'
+
+const DEFAULT_PAGE_SIZE = 35
 
 export default async function Content({
     searchParams,
@@ -12,10 +15,8 @@ export default async function Content({
     className?: string
 }) {
 
-    const defaultPageSize = 35
-
     const page = searchParams['page'] ?? '1'
-    const per = searchParams['per'] ?? defaultPageSize
+    const per = searchParams['per'] ?? DEFAULT_PAGE_SIZE
     const search = searchParams['search'] as string | undefined
 
     const [ dataResult ] = await Promise.allSettled([
@@ -41,24 +42,34 @@ export default async function Content({
                 {dataResult.value.data.map(sportsman => {
                     const firstImage = sportsman.attributes.images.data.length > 0 ? sportsman.attributes.images.data[0].attributes.url : undefined
                     return (
-                        <div key={sportsman.id} className='text-center'>
-                            <Link href={`/sportsmans/${sportsman.id}`} className='font-semibold lg:text-base text-sm hover:text-accent-orange transition-all'>
-                                <ImageComponent 
-                                    src={firstImage}
-                                    alt="Фото"
-                                    fill={false}
-                                    width={150}
-                                    height={150}
-                                    className='mx-auto aspect-square object-cover rounded-2xl mb-2'
-                                />
-                                {sportsman.attributes.fio}
-                            </Link>
-                            <h2 className='font-medium lg:text-sm text-xs text-primary-foreground mt-1'>{sportsman.attributes.sport.data?.attributes.title}</h2>
-                            <h3 className='lg:text-sm text-xs'>{sportsman.attributes.rank}</h3>
-                        </div>
+                        <Link 
+                            key={sportsman.id}
+                            href={`/sportsmans/${sportsman.id}`} 
+                            className='lg:text-base text-sm text-center rounded-3xl bg-primary shadow-md hover:scale-105 hover:shadow-lg transition-all duration-300'
+                        >
+                            <ImageComponent 
+                                src={firstImage}
+                                alt="Фото"
+                                fill={false}
+                                width={600}
+                                height={600}
+                                className='w-full md:aspect-[5/6] aspect-square mx-auto object-cover rounded-t-3xl'
+                            />
+                            <div className='p-3'>
+                                <h2 className='font-medium mb-0.5'>{sportsman.attributes.fio}</h2>
+                                <h3 className='text-xs'>{sportsman.attributes.rank}</h3>
+                            </div>
+                        </Link>
                     )}
                 )}
             </div>
+            <PaginationControls
+                length={dataResult.value.meta.pagination.total}
+                defaultPageSize={DEFAULT_PAGE_SIZE}
+                pageParam='page'
+                perParam='per'
+                showMore={false}
+            />
         </div>
     )
 }
