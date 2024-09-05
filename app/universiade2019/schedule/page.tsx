c17249dd448db87de49d9f0b53cccadd1fcf5b26
TaisTais <tais.tais205@mail.ru>
2024-09-06 00:01:58 +1000

    feat: competition schedule page

--- app/universiade2019/schedule/page.tsx
+++ app/universiade2019/schedule/page.tsx
@@ -1,45 +1,62 @@
 import Breadcrumbs from '@/components/Breadcrumbs'
-import Markdown from '@/components/Markdown'
 import React from 'react'
+import { getCompetitionSchedule } from '@/lib/queries/universiade/getCompetitionSchedule';
+import ErrorHandler from '@/components/errors/ErrorHandler';
+import Image from "next/image";
 
-export default function Schedule() {
 
-    const text = `
-## **Расписание соревнований Универсиады-2019:**
+export default async function Schedule() {
 
-- соревнования по горнолыжному спорту проходили со 2 по 11 марта 2019 года в Фанпарке «Бобровый лог»;
+  const [ dataResult ] = await Promise.allSettled([
+    getCompetitionSchedule()
+  ]);
+  if (dataResult.status === "rejected") return (
+    <ErrorHandler
+      error={dataResult.reason as unknown}
+      place="Расписание соревнований" 
+      notFound
+      goBack={false}
+    />
+  );
 
-- соревнования по сноуборду и фристайлы проходили с 1 по 11 марта 2019 года на кластере «Сопка»;
-
-- соревнования по спортивному ориентированию и лыжным гонкам проходили с 1 по 12 марта 2019 года на кластере «Радуга»;
-
-- соревнования по биатлону проходили с 4 по 10 марта 2019 года в спортивном комплексе «Академия биатлона»;
-
-- соревнования по керлингу проходили с 3 по 10 марта 2019 года во Дворце спорта им. Ивана Ярыгина;
-
-- соревнования по шорт-треку проходили с 4 по 6 марта 2019 года в многофункциональном спортивном комплексе «Арена.Север»;
-
-- за мужской и женский хоккей с мячом можно было поболеть с 1 по 10 марта 2019 года на стадионе «Енисей»;
-
-- отдельно мужчины играли в хоккей с 1 по 12 марта 2019 года в ледовом дворце «Кристалл Арена»;
-
-- увидеть женский хоккей можно было сразу на двух объектах: с 1 по 8 марта 2019 года на стадионе «Первомайский» и с 10 по 11 марта 2019 года в многофункциональном спортивном комплексе «Арена.Север»;
-
-- соревнования по фигурному катанию проходили с 6 по 9 марта 2019 года в спортивно-зрелищном комплексе «Платинум Арена Красноярск».
-`
-
-    return (
-        <div className='sm:w-4/5 container my-16'>
-            <Breadcrumbs data={[
-              {title: "Универсиада", slug: "universiade2019"}, 
-              {title: "Расписание соревнований", slug: "schedule" }
-            ]}/>
-            <div className='flex lg:flex-row flex-col justify-between gap-6 border-b-2 border-foreground pb-2'>
-                <h1 className='font-semibold lg:text-xl text-base'>Расписание соревнований</h1>
+  return (
+    <div className='sm:w-4/5 container my-16'>
+      <Breadcrumbs data={[
+        {title: "Универсиада", slug: "universiade2019"}, 
+        {title: "Расписание соревнований", slug: "schedule" }
+      ]}/>
+      <div className='flex lg:flex-row flex-col justify-between gap-6 border-b-2 border-foreground pb-2'>
+          <h1 className='font-semibold lg:text-xl text-base'>{dataResult.value.title}</h1>
+      </div>
+      <div className='mt-10'>
+        {dataResult.value.competitions.map((comp, index) => 
+        <div key={index} className='mb-6 py-8 px-12 bg-gradient-to-r from-gradient-sky to-gradient-pink w-full rounded-2xl'>
+          <h2 className='text-xl font-semibold text-primary-foreground mb-4'>{comp.title}</h2>
+          <div className='flex flex-row gap-16'>
+            <div className='flex flex-row gap-2 items-center'>
+              <Image
+                src='/icons/calendar.png' 
+                alt='universiade main figures'
+                width={25}
+                height={25}
+                className="w-5 h-5"
+              />
+              <p>{comp.date}</p>
             </div>
-            <div className='mt-16'>
-                <Markdown data={text} moreButton={false}/>
+            <div className='flex flex-row gap-1.5'>
+              <Image
+                src='/icons/marker.png' 
+                alt='universiade main figures'
+                width={25}
+                height={25}
+                className="w-5 h-5 items-center"
+              />
+              <p>{comp.address}</p>
             </div>
+          </div>
         </div>
-    )
+        )}
+      </div>
+    </div>
+  )
 }
